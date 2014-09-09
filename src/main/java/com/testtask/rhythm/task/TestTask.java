efe822829ee31aa3760620609ef5368a65d9e463
IaroslavBegemot <sir_kot@ukr.net>
2014-09-09 19:27:00 +0300

    new

---
+++ src/main/java/com/testtask/rhythm/task/TestTask.java
@@ -0,0 +1,9 @@
+package com.testtask.rhythm.task;
+
+import java.util.List;
+
+public interface TestTask {
+	
+	public List<Integer> findHighestSums(int[][] lists, int n);
+
+}
---
+++ src/main/java/com/testtask/rhythm/task/impl/TestTaskImpl.java
@@ -0,0 +1,14 @@
+package com.testtask.rhythm.task.impl;
+
+import java.util.List;
+
+import com.testtask.rhythm.task.TestTask;
+
+public class TestTaskImpl implements TestTask {
+
+	public List<Integer> findHighestSums(int[][] lists, int n) {
+
+		return null;
+	}
+
+}
