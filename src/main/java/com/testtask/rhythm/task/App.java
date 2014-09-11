581743c4db18538d97eb05e4566736e7b34fbab3
IaroslavBegemot <sir_kot@ukr.net>
2014-09-12 02:14:16 +0300

    added

--- src/main/java/com/testtask/rhythm/task/App.java
+++ src/main/java/com/testtask/rhythm/task/App.java
@@ -1,15 +1,63 @@
 package com.testtask.rhythm.task;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.testtask.rhythm.task.impl.TestTaskImpl2;
+import com.testtask.rhythm.task.utils.MergeSort;
 
 public class App {
 	public static void main(String[] args) {
 		
-		int[][] lists = {{4,1},{5,0,0},{5,4,3,2,1},{6,4,2},{1}};
+		final int nm = 1000;
+
+		// int[][] lists = { { 4, -1 }, { 5, 0, 0 }, { 5, 4, 3, 2, 1 }, { 6, 4, 2 },
+		// { 1 } };
+
+		int[][] lists = { { 4, -1 }, { 5, 0, 0 }, { 5, 4, -1, -2, -3 }, { 6, 4, 2 }, { 1 }, { 0, -1, -2, -3, -4 } };
+		TestTask task = new TestTaskImpl2();
+
+		List<Integer> result = task.findHighestSums(lists.clone(), 100);
+		System.out.println("findHighestSums(lists, 100) = " + result);
+
+		result = task.findHighestSums(lists.clone(), 5);
+		System.out.println("findHighestSums(lists, 5) = " + result);
+
+//		 Random integer test
+		 Random random = new Random();
 		
-		TestTaskImpl2 task = new TestTaskImpl2();
+		 List<int[]> outer = new ArrayList<int[]>();
 		
-		task.test(lists);
+		 int rows = random.nextInt(nm) + 1;
+		 System.out.println("Random generation start");
+		
+		 for (int i = 0; i < rows; i++) {
+		 int cols = random.nextInt(nm) + 1;
+		
+		 int[] inner = new int[cols];
+		 for (int j = 0; j < cols; j++) {
+		 inner[j] = random.nextInt(100)-50;
+		 }
+		 int[] innerArray = MergeSort.mergeSort(inner);
+		 outer.add(innerArray);
+		 }
+		
+		 lists = new int[outer.size()][];
+		 lists = outer.toArray(lists);
+		
+		 System.out.println("Random generation end");
+		 System.out.println("Random generated lists");
+		
+		 for (int i = 0; i < lists.length; i++) {
+		 for (int j = 0; j < lists[i].length; j++) {
+		 System.out.format("%4d", lists[i][j]);
+		 }
+		 System.out.println();
+		 }
+		
+		 result = task.findHighestSums(lists.clone(), 100);
+		 System.out.print("result = " + result);
 
 	}
 }
--- src/main/java/com/testtask/rhythm/task/impl/TestTaskImpl2.java
+++ src/main/java/com/testtask/rhythm/task/impl/TestTaskImpl2.java
@@ -1,5 +1,6 @@
 package com.testtask.rhythm.task.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.testtask.rhythm.task.TestTask;
@@ -8,33 +9,75 @@ public class TestTaskImpl2 implements TestTask {
 
 	public List<Integer> findHighestSums(int[][] lists, int n) {
 
-		return null;
-	}
+		List<Integer> result = new ArrayList<Integer>();// result
+		List<Integer> firstColumn = new ArrayList<Integer>();// first column of inputed matrix
 
-	public void test(int[][] lists) {
-		int[] column = new int[lists.length];
-		for (int i = 0, j = 1; i < lists.length; j++) {
-			for (; j < lists[i].length; i++) {
-				column[i] = lists[i][0] - lists[i][j];
-				System.out.println("column[i] = " + column[i]);
+		List<Integer> diffColumn = getFirstAndDiffColumnAndFirstHighestSum(lists, result, firstColumn);// column with difference from highest sum
 
-			}
+		int i = 0;
+		while (i < n - 1 && !diffColumn.isEmpty()) {// iterate over amount of needed highest sums or return all available (first highest sum
+																								// we already have) 
+			int minRowIndex = findMinColumnIndex(diffColumn);// finding index of row with minimum value of odds
+			iteration(result, diffColumn, firstColumn, lists, minRowIndex);// main body of algorithm
+			i++;
 		}
-		int minCol = findMinColumnIndex(column);
 
-		System.out.println("column = " + column);
-		System.out.println("minCol = " + minCol);
-
+		return result;
 	}
 
-	private int findMinColumnIndex(int[] a) {
+	private List<Integer> getFirstAndDiffColumnAndFirstHighestSum(int[][] lists, List<Integer> result, List<Integer> firstColumn) {
+		List<Integer> diffColumn = new ArrayList<Integer>();
+		int summ = 0;
+
+		for (int i = 0; i < lists.length; i++) {// iterate over columns, filling first column and calculating first highest sum
+			firstColumn.add(lists[i][0]);
+			summ += lists[i][0];
+
+			if (lists[i].length <= 1) { // if row have only one element than going to next row
+				continue;
+			}
+			if(lists[i][1] > 0){
+				diffColumn.add(lists[i][0] - lists[i][1]);// filling column with difference from highest sum
+			} else {
+				diffColumn.add(lists[i][0] + Math.abs(lists[i][1]));
+			}
+			
+		}
+		result.add(summ);
+		return diffColumn;
+	}
+
+	private void iteration(List<Integer> result, List<Integer> diffColumn, List<Integer> firstColumn, int[][] input, int rowIndex) {
+
+		result.add(result.get(0) - diffColumn.get(rowIndex)); // calculating highest sum
+
+		if (input[rowIndex].length > 0) { // if row have more than one element than remove first element in row
+			int[] temp = new int[input[rowIndex].length - 1];
+			System.arraycopy(input[rowIndex], 1, temp, 0, temp.length);
+			input[rowIndex] = temp;
+		}
+
+		if (input[rowIndex].length > 1) {// if row have more than two elements calculate difference, otherwise remove
+			if(input[rowIndex][1] > 0){
+				diffColumn.set(rowIndex, firstColumn.get(rowIndex) - input[rowIndex][1]);
+			} else {
+					diffColumn.add(firstColumn.get(rowIndex) + Math.abs(input[rowIndex][1]));
+				}
+		} else {
+			diffColumn.remove(rowIndex);
+		}
+	}
+
+	private int findMinColumnIndex(List<Integer> a) {
 		int minIndex = 0;
 		int m = Integer.MAX_VALUE;
-		for (int i = 0; i < a.length; i++) {
-			// m = m <= a[i] ? m : a[i];
-			minIndex = m <= a[i] ? minIndex : i;
-
+		for (int i = 0; i < a.size(); i++) {
+			if (m > a.get(i)) {
+				minIndex = i;
+				m = a.get(i);
+			}
 		}
 		return minIndex;
 	}
+
 }
